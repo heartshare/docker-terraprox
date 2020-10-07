50ea47546ef8592d90b78ab4db65180ff2c2395e
Gerhard Lausser <gerhard.lausser@consol.de>
2020-10-07 13:25:49 +0200

    fix runtf.sh

--- runtf.sh
+++ runtf.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-maintf=`find / -name main.tf 2>/dev/null | head -1`
 mkdir work
+maintf=`find / -name main.tf 2>/dev/null | head -1`
 cp -r `dirname $maintf`/* work
 cd work
 test -f id_rsa && chmod 600 id_rsa
