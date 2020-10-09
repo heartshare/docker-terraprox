e658d9924508a43ce895a82b2f11ac7122f42abd
Gerhard Lausser <gerhard.lausser@consol.de>
2020-10-09 19:09:24 +0200

    add a consul backend if CONSUL_ADDRESS is set

--- runtf.sh
+++ runtf.sh
@@ -2,8 +2,23 @@
 # point to consul
 
 distri=${DISTRIBUTION:-centos-7.7}
-uuid=$(uuidgen)
-uuid=${uuid%%-*}
+if [ -z "$UNIQUE_TAG" ]; then
+  uuid=$(uuidgen)
+  uuid=${uuid%%-*}
+else
+  uuid=${UNIQUE_TAG}
+fi
 
+if [ -n "$CONSUL_ADDRESS" ]; then
+  cat > backend.tf <<EOTF
+terraform {
+  backend "consul" {
+    address = "${CONSUL_ADDRESS}"
+    scheme  = "http"
+    path    = "build/${distri}/${uuid}"
+  }
+}
+EOTF
+fi
 terraform init
 terraform $* -var ssh_password="$SSH_PASSWORD" -var vm_clone=t-${distri} -var vm_name=b-omd-${distri}-${uuid} -var cpu_sockets=2 -var cpu_cores=4 -var memory=32768 --auto-approve
