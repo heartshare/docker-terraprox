0b516e18deb7ca75495ea11b0b967b885af68874
Gerhard Lausser <gerhard.lausser@consol.de>
2020-10-24 02:17:51 +0200

    let terraform register the vm address in consul

--- runtf.sh
+++ runtf.sh
@@ -19,6 +19,27 @@ terraform {
   }
 }
 EOTF
+  cat > consul_node.tf <<EOTF
+provider "consul" {
+  address    = "${CONSUL_ADDRESS}"
+  #datacenter = "nyc1"
+}
+
+resource "consul_keys" "nslookup" {
+  key {
+    name   = "address"
+    path   = "nslookup/\${var.vm_uuid}"
+    value  = proxmox_vm_qemu.cloudinit-vm.ssh_host
+    delete = true
+  }
+}
+
+resource "consul_node" "hostname" {
+  name    = var.vm_uuid
+  address = proxmox_vm_qemu.cloudinit-vm.ssh_host
+}
+
+EOTF
 fi
 terraform init
 terraform $* -var ssh_password="$SSH_PASSWORD" -var vm_clone=t-${distri} -var vm_name=b-omd-${distri}-${uuid} -var cpu_sockets=2 -var cpu_cores=4 -var memory=32768 --auto-approve
